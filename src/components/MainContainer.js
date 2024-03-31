0c0f723de06d663d5b9e62af3e17c97930aa0d43
Varsha Rani <varsharanijnv16122001@gmail.com>
2024-03-31 18:19:51 +0530

    first commit

---
+++ src/components/MainContainer.js
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
+
+const MainContainer = () => {
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+
+  // console.log(movies);
+
+  if (!movies) return;
+
+  const mainMovie = movies[0];
+
+  const { original_title, overview, id } = mainMovie;
+
+  // console.log(id);
+
+  return (
+    <div className="pt-[30%] bg-black md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
+    </div>
+  );
+};
+export default MainContainer;
